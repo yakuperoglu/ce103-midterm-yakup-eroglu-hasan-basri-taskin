d2f71395141d259147352489c0036131c0989290
HBasriTaskin <taskin.hsnbasri@gmail.com>
2023-11-15 15:42:26 +0300

    Update were made for unit tests

--- src/yachtsystem/header/yachtsystem.h
+++ src/yachtsystem/header/yachtsystem.h
@@ -1,57 +1,43 @@
 /**
  * @file yachtsystem.h
  *
- * @brief Provides functions for math. utilities
+ * @brief Provides functions for yacht system utilities
  */
 
 #ifndef YACHTSYSTEM_H
 #define YACHTSYSTEM_H
 
 #include "../../utility/header/commonTypes.h"
+#include <string>
+
 using namespace std;
 
 namespace Coruh {
-namespace Yachtsystem {
-struct SeaVehicle {
-  void setName(const string &name);  // Set the name of the yacht
-  void setLength(int length);         // Set the length of the yacht
-  void setMaintanceStatus(string &input);         // Set the maintance status of yacht
-  void setOwner(string &name);         // Set the length of the yacht
-  //We setted const this function because this function just read our variables. It cannot be changed with this function.
-  void displayInfo() const;              // Display yacht information
-  void displayExtendedInfo() const;              // Display yacht information
+	namespace Yachtsystem {
+		struct SeaVehicle {
+			bool setName(const string& name);  // Set the name of the yacht
+			bool setLength(int length);            // Set the length of the yacht
+			bool setMaintanceStatus(const string& input);   // Set the maintance status of yacht
+			bool setOwner(const string& name);    // Set the owner of the yacht
+			int displayInfo() const;            // Display yacht information
+			int displayExtendedInfo() const;       // Display extended yacht information
 
+		private:
+			bool maintanceStatus;
+			string name;
+			int length;
+			string owner;
+		};
 
- private:
-  bool maintanceStatus;
-  string name;
-  int length;
-  string owner;
-};
+		struct Reservation {
+			bool setCustomerName(const string& name);   // Set the customer's name
+			int displayInfo() const;       // Display reservation information
 
-struct Harbor {
-  void setLocation(const string &location); // Set the location of the harbor
-  void setCapacity(int capacity);              // Set the capacity of the harbor
-  //We setted const this function because this function just read our variables. It cannot be changed with this function.
-  void displayInfo() const;                     // Display harbor information
-
- private:
-  string location;
-  int capacity;
-};
-
-struct Reservation {
-  void setCustomerName(const string &name);  // Set the customer's name
-  void setReservationDate(const string &date); // Set the reservation date
-  //We setted const this function because this function just read our variables. It cannot be changed with this function.
-  void displayInfo() const;                         // Display reservation information
-
-
- private:
-  string customerName;
-  string reservationDate;
-};
-}
+		private:
+			string customerName;
+			string reservationDate;
+		};
+	}
 }
 
 #endif // YACHTSYSTEM_H
--- src/yachtsystem/src/yachtsystem.cpp
+++ src/yachtsystem/src/yachtsystem.cpp
@@ -2,56 +2,68 @@
 #include <iostream>
 
 using namespace Coruh::Yachtsystem;
-using namespace std;
-// Yachtsystem.cpp
 
-// SeaVehicle class methods
-void SeaVehicle::setName(const string& name) {
-	this->name = name; //The value used to determine the name of the yacht
+// SeaVehicle structs methods
+bool SeaVehicle::setName(const string& name) {
+	//name couldnt empty
+	if (name.empty())
+		return false;
+
+	this->name = name;
+	return true;
 }
 
-void SeaVehicle::setLength(int length) {
-	this->length = length; //The value used to determine the length of the yacht
+bool SeaVehicle::setLength(int length) {
+	//length couldn't lower or equal to 0
+	if (length <= 0)
+		return false;
+
+	this->length = length;
+	return true;
 }
 
-void SeaVehicle::displayInfo() const {
-	cout << "SeaVehicle Name: " << name << ", Length: " << length << ", SeaVehicle owner: " << owner; //Displays the name, length and yacht owner values ​​of the SeaVehicle
+bool SeaVehicle::setMaintanceStatus(const string& input) {
+	//input must be just yes or no, other values is incorrect.
+	if (input != "yes" && input != "no")
+		return false;
+
+	this->maintanceStatus = (input == "yes");
+	return true;
 }
 
-void SeaVehicle::displayExtendedInfo() const {
-	cout << name  << " | " << length << " | " << owner << " | " << (maintanceStatus == true ? "Maintance necessary" : "Not necessary") << endl; //Displays the name, length, yachtOwner and maintanceStauts values ​​of the SeaVehicle
+bool SeaVehicle::setOwner(const string& ownerName) {
+	//owner value cant be null.
+	if (ownerName.empty())
+		return false;
+
+	this->owner = ownerName;
+	return true;
 }
 
-void SeaVehicle::setMaintanceStatus(string& input) {
-	this->maintanceStatus = (input == "yes" ? true : false);  //The value used to determine the status of the yacht
+int SeaVehicle::displayInfo() const {
+	//its just show the info 
+	cout << "SeaVehicle Name: " << name << ", Length: " << length << ", SeaVehicle owner: " << owner << endl;
+	return 0;
 }
 
-void SeaVehicle::setOwner(string& ownerName) {
-	this->owner = ownerName;//The value used to determine the owener of the yacht
+int SeaVehicle::displayExtendedInfo() const {
+	//its just show the info 
+	cout << name << " | " << length << " | " << owner << " | " << (maintanceStatus ? "Maintance necessary" : "Not necessary") << endl;
+	return 0;
 }
 
-// Harbor class methods
-void Harbor::setLocation(const string& location) {
-	this->location = location; //The value that determines the location of the Harbor
+// Reservation Struct methods
+bool Reservation::setCustomerName(const string& name) {
+	//customer name cant be empty
+	if (name.empty())
+		return false;
+
+	this->customerName = name;
+	return true;
 }
 
-void Harbor::setCapacity(int capacity) {
-	this->capacity = capacity; //The value that determines the capacity of the Harbor
-}
-
-void Harbor::displayInfo() const {
-	cout << "Harbor Location: " << location << ", Capacity: " << capacity; //Displays the location and capacity values of the Harbor
-}
-
-// Reservation class methods
-void Reservation::setCustomerName(const string& name) {
-	this->customerName = name; //The value used to determine the name of the customer
-}
-
-void Reservation::setReservationDate(const string& date) {
-	this->reservationDate = date; //The value used to determine the date of the reservation
-}
-
-void Reservation::displayInfo() const {
-	cout << "Reservation Name: " << customerName << ", Date: " << reservationDate; //Displays the customerName and reservarionDate
+int Reservation::displayInfo() const {
+	//its just show the info 
+	cout << "Reservation Name: " << customerName << ", Date: " << reservationDate << endl;
+	return 0;
 }
